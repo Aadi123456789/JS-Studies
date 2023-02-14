aa621aa0c1d2b1b051e4cba91f875525a7ff2d21
AADITHYA.M <104914794+Aadi123456789@users.noreply.github.com>
2023-02-14 11:48:57 +0530

    Add files via upload

---
+++ js (3 problems)/oddishorevenish/oddishorevenish.js
@@ -0,0 +1,32 @@
+function oddishorevenish(num)
+
+{
+let sum=0;
+    
+while(num>0)
+    
+{
+        
+sum+=num%10;
+        
+sum=Math.floor(sum);
+        
+num/=10;
+    
+}
+if(sum%2==0)
+return "evenish"
+    
+else return "oddish";
+
+}
+
+console.log(oddishorevenish(121));
+
+console.log(oddishorevenish(41));
+
+console.log(oddishorevenish(501));
+
+console.log(oddishorevenish(373));
+
+console.log(oddishorevenish(444));
