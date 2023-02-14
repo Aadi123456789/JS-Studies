1ba8bdfa77e1c73283d3db888e181d6827792df0
AADITHYA.M <104914794+Aadi123456789@users.noreply.github.com>
2023-02-14 11:36:43 +0530

    Add files via upload

---
+++ js (3 problems)/1darray/1darray.js
@@ -0,0 +1,15 @@
+function con(...arr)
+
+{
+   
+return ("["+[].concat(arr).join(',')+"]");
+
+}
+
+console.log(con([1,2,3,4],[8,10]))
+;
+console.log(con([1,2,3],[4,5],[6,7]));
+
+console.log(con([1,2],[3,4]));
+
+console.log(con([1],[2],[3],[4],[5],[6],[7]));
